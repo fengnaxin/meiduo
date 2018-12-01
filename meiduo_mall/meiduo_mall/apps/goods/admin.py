253bfe2d6c462293d3ed1d5f934f3a7bec01bf25
fengnaxin <520kiss@gmai.com>
2018-12-01 16:36:45 +0800

    详情页面静态化

--- meiduo_mall/meiduo_mall/apps/goods/admin.py
+++ meiduo_mall/meiduo_mall/apps/goods/admin.py
@@ -1,10 +1,49 @@
 from django.contrib import admin
 
-from celery_tasks.html.tasks import generate_static_list_search_html
+from celery_tasks.html.tasks import generate_static_list_search_html, generate_static_sku_detail_html
 from . import models
 
 
 # Register your models here.
+
+class SKUAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        """
+        :param request: 本次请求的对象
+        :param obj: 本次保存的模型类对象
+        :param form:本次保存的表单内容
+        :param change: 本次保存与之前的数据变化
+        :return: None
+        """
+        obj.save()
+        generate_static_sku_detail_html.delay(obj.id)
+
+
+class SKUImageAdmin(admin.ModelAdmin):
+
+    def save_model(self, request, obj, form, change):
+        """
+        :param request: 本次请求的对象
+        :param obj: 本次保存的模型类对象
+        :param form:本次保存的表单内容
+        :param change: 本次保存与之前的数据变化
+        :return: None
+        """
+        obj.save()
+        generate_static_list_search_html.delay()
+
+    def delete_model(self, request, obj):
+        """
+        :param request: 本次请求的对象
+        :param obj: 本次删除的模型类对象
+        :param form:本次删除的表单内容
+        :param change: 本次删除与之前的数据变化
+        :return: None
+        """
+        obj.delete()
+        generate_static_list_search_html.delay(obj.sku.id)
+
+
 class GoodsCategoryAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         """
@@ -35,6 +74,6 @@ admin.site.register(models.Goods)
 admin.site.register(models.Brand)
 admin.site.register(models.GoodsSpecification)
 admin.site.register(models.SpecificationOption)
-admin.site.register(models.SKU)
+admin.site.register(models.SKU, SKUAdmin)
 admin.site.register(models.SKUSpecification)
-admin.site.register(models.SKUImage)
+admin.site.register(models.SKUImage,SKUImageAdmin)
